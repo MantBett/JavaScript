8b1d678b16f34aaae2c1a630289a3b3fe3136e6c
Ana Beatriz Mantovani Bett <abmbett@hotmail.com>
2020-05-18 14:46:11 -0300

    16/05

---
+++ lessons/11/ex008.js
@@ -0,0 +1,15 @@
+var vel = 90;
+console.log(`A velocidade do sei carro é ${vel} Km/h`);
+
+/*
+      Condição simples
+if (vel > 60){
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+*/
+
+if (vel > 60){
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+}
+
+console.log('Dirija sempre usando sinto de segurança!');
